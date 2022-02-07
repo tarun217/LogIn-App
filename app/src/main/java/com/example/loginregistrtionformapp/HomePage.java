f9728c6cb8e5aec10ae8ca461b00f9fbf3db0c3e
Tarun217 <tarun.patel@sayalife.in>
2022-02-07 15:18:19 +0530

    LogIn Registration form

--- app/src/main/java/com/example/loginregistrtionformapp/HomePage.java
+++ app/src/main/java/com/example/loginregistrtionformapp/HomePage.java
@@ -1,14 +1,12 @@
+
 package com.example.loginregistrtionformapp;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -17,11 +15,6 @@ public class HomePage extends AppCompatActivity {
     TabLayout tab_layout;
     ViewPager2 pager2;
     pageAdapter adapter;
-
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
